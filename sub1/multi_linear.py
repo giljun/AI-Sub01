1b88fe3d9a3291feb68613cbae88d1f0a55eb1b9
박길준 <adda281@naver.com>
2019-08-23 09:44:32 +0900

    S1P2241002-11|2019/08/23|학습된 모델 저장 기능 구현

--- sub1/multi_linear.py
+++ sub1/multi_linear.py
@@ -83,7 +83,7 @@ print("예상 판매량: {}".format(expected_sales(
        float(X_test[3][1]), float(X_test[3][1]), float(X_test[3][2]), beta_0, beta_1, beta_2, beta_3)))
 
 """
-Req. 1-5. pickle로 lrmodel 데이터 저장
+Req. 1-5. pickle로 lrmodel 데이터 저장(완료)
 파일명: model.clf
 """
 with open('model.clf', 'wb') as fout:
